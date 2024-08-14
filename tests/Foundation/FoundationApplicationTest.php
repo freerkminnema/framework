fc96a091a8e0bcfc0f65742573d002e2b6f83080
Freerk Minnema <41089591+freerkminnema@users.noreply.github.com>
2024-08-14 12:50:56 +0200

    added tests for the simple $scoped binding syntax

--- tests/Foundation/FoundationApplicationTest.php
+++ tests/Foundation/FoundationApplicationTest.php
@@ -63,6 +63,34 @@ class FoundationApplicationTest extends TestCase
         $this->assertNotSame($instance, $app->make(AbstractClass::class));
     }
 
+    public function testScopedBindingsAreCreatedWhenServiceProviderIsRegistered()
+    {
+        $app = new Application;
+        $app->register($provider = new class($app) extends ServiceProvider
+        {
+            public $scoped = [
+                NonContractBackedClass::class,
+                AbstractClass::class => ConcreteClass::class,
+            ];
+        });
+
+        $this->assertArrayHasKey(get_class($provider), $app->getLoadedProviders());
+
+        $instance = $app->make(AbstractClass::class);
+
+        $this->assertInstanceOf(ConcreteClass::class, $instance);
+        $this->assertSame($instance, $app->make(AbstractClass::class));
+        $app->resetScope();
+        $this->assertNotSame($instance, $app->make(AbstractClass::class));
+
+        $instance = $app->make(NonContractBackedClass::class);
+
+        $this->assertInstanceOf(NonContractBackedClass::class, $instance);
+        $this->assertSame($instance, $app->make(NonContractBackedClass::class));
+        $app->resetScope();
+        $this->assertNotSame($instance, $app->make(AbstractClass::class));
+    }
+
     public function testSingletonsAreCreatedWhenServiceProviderIsRegistered()
     {
         $app = new Application;
